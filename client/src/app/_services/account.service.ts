e6de310fc154be876faa677a967bd34d39b14d8c
Dawid Januszkiewicz <djanuszkiewicz147@gmail.com>
2023-03-30 19:41:15 +0200

    Photo manipulation

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -29,6 +28,7 @@ export class AccountService {
 
   setCurrentUser(user: User)
   {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
@@ -38,8 +38,7 @@ export class AccountService {
       map(user => {
         if (user)
         {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
 
       })
